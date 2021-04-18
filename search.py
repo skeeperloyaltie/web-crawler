3465a53e908d6df4bb4d26e5ca05a4ee7c4f0fa1
sk33l0y4 <sk33l0y4@localhost.localdomain>
2021-04-18 17:37:01 +0300

    web-crawler

---
+++ search.py
@@ -0,0 +1,13 @@
+
+import scrapy
+import inverted_index
+
+
+class CU_INDEX(scrapy.Spider):
+    name = "CU_INDEX"
+    start_urls = ['https://scholar.google.co.uk/citations?view_op=view_org&hl=en&org=9117984065169182779']
+
+	
+
+
+
